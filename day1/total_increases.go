fc9a2175e07f9eb8dc448dbd5ad1050b2c2ed944
Dan Tcheche <dtchechenistky@devartis.com>
2021-12-03 12:00:28 -0300

    Count total increases

--- day1/total_increases.go
+++ day1/total_increases.go
@@ -2,15 +2,25 @@ package day1
 
 import (
 	"advent-of-code-2021/utils"
-	"fmt"
+	"strconv"
 )
 
 var input = utils.GetFileData("./day1/input.txt")
 
 func TotalIncreases() int {
 	totalIncreases := 0
+	previousValue := -1
 	for _, value := range input {
-		continue
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			panic("Invalid input")
+		}
+		if previousValue > 0 {
+			if intValue > previousValue {
+				totalIncreases += 1
+			}
+		}
+		previousValue = intValue
 	}
 	return totalIncreases
 }
